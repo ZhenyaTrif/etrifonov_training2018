a04f10966e134be8042fff15c2a2586d57005212
Eugene Trifonov <eugene_trifonov@bitbucket.org>
2018-11-04 17:41:38 +0300

    HW14

---
+++ MVC/HW14/HW14/App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HW14
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute("Home/Contact", "Help/{id}", new { controller = "Home", action = "Contact", id = UrlParameter.Optional });
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Library", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MVC/HW14/HW14/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HW14.MvcApplication" Language="C#" %>
